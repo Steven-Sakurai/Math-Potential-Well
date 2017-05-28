60130ab19d8df79a32da193d136e635b7f305207
Steven-Sakurai <1500015941@pku.edu.cn>
2017-05-28 14:17:30 +0800

    update from home

--- tsa_project/play/month_temp.r
+++
@@ -1,6 +0,0 @@
-library("curl")
-tmpf <- tempfile()
-curl_download("http://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/time_series/HadCRUT.4.5.0.0.monthly_ns_avg.txt", tmpf)
-gtemp <- read.table(tmpf, colClasses = rep("numeric", 12))[, 1:2] # only want some of the variables
-names(gtemp) <- c("time", "Temperature")
-
